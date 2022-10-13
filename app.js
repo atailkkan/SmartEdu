7cc7683f7579d060691c529d1afa5e74e3488125
atailkkan <atailkkan@gmail.com>
2022-10-13 17:42:26 +0300

    Smart Edu Uploaded

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
-
 const app = express();
+
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.status(200).render('index', { pageName: 'index' });
+});
+app.get('/about', (req, res) => {
+    res.status(200).render('about', { pageName: 'about' });
+});
 
 const port = 3000;
 app.listen(port, () => {
